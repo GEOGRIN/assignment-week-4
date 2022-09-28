11701b16a219538aac47218deaa7f79eabfcd379
geogrin <arogundade_yinka@yahoo.com>
2022-09-28 00:42:25 -0700

    add index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+function person (){
+
+return {
+    firstname : "Brad",
+    lastname : "Funsho",
+    gender : "male",
+    tribe : "yoruba",
+    school : "futa",
+    wife : "Bimbola",
+    age : 32,
+    run : function(){
+        console.log ("run")
+    },
+    dance : function (){
+        console.log ("dance")
+    },
+    sing : function (){
+        console.log ("sing")
+    },
+    write : function (){
+        console.log ("write")
+    }
+
+},
+}
